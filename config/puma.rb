28d71c39f61f0073558966b3918283f18228f005
Sergey Mavrin <sergey.mavrin@onetwotrip.com>
2019-02-04 18:20:28 +0300

    Add puma control/status settings

--- config/puma.rb
+++ config/puma.rb
@@ -4,6 +4,10 @@ threads         threads_count, threads_count
 bind_address  = Postal.config.web_server&.bind_address || '127.0.0.1'
 bind_port     = Postal.config.web_server&.port&.to_i || 5000
 bind            "tcp://#{bind_address}:#{bind_port}"
+control_address = Postal.config.web_server&.control_address || '127.0.0.1'
+control_port    = Postal.config.web_server&.control_port&.to_i || '9293'
+control_token   = Postal.config.web_server&.control_token || 'foo'
+activate_control_app "tcp://#{control_address}:#{control_port}", { auth_token: control_token}
 environment     Postal.config.rails&.environment || 'development'
 prune_bundler
 quiet false
